121e3d290e5280b8814216b075bd113fb537f6d3
liltechnomancer <levi@technomancy.dev>
2026-03-17 12:00:56 +0000

    test(one-key-core): add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,8 @@ export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
     exclude: ['**/node_modules/**'],
+    coverage: {
+      provider: 'v8' // or 'istanbul'
+    },
   },
 })
